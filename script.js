7c30cc1fddabba05cbd21cb0e20c22c4fc194e04
Bobzero <rajabu.mustafa.00@gmail.com>
2023-07-18 13:54:42 +0300

    project completed

---
+++ script.js
@@ -0,0 +1,22 @@
+const btnEl = document.getElementById("btn");
+const apiKey = "T567QOjUYwtyBCvPJOFiHg==sFoo42jg6qFxi6j3";
+const apiURL = "https://api.api-ninjas.com/v1/jokes?limit=";
+const jokeEl = document.getElementById("joke");
+
+
+const options = {
+    method: "GET",
+    headers: {
+        "X-Api-Key": apiKey,
+    },
+}
+
+async function getJoke() {
+    jokeEl.textContent = "loading..."
+    const response = await fetch(apiURL, options)
+    const data = await response.json()
+    const joke = data[0].joke;
+    jokeEl.textContent = joke;
+}
+
+btnEl.addEventListener("click", getJoke);
